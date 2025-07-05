440c4bf016e22ebebecf3ed7de25100051b5281c
Sayan Chakraborty <g24ai1062@iitj.ac.in>
2025-07-05 11:29:26 +0530

    Fix: Add target_column parameter to split_data function

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
-from sklearn.datasets import fetch_california_housing
+kfrom sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
 import pandas as pd
 
 def load_data():
@@ -9,11 +10,12 @@ def load_data():
     data = housing.frame
     return data
 
-def split_data(data):
+def split_data(data, target_column):
     """
-    Split the data into features and target.
+    Split the data into training and testing sets.
     """
-    X = data.drop('MedHouseVal', axis=1)
-    y = data['MedHouseVal']
-    return X, y
+    X = data.drop(target_column, axis=1)
+    y = data[target_column]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    return X_train, X_test, y_train, y_test
 
